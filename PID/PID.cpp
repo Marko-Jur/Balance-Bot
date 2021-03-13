373e56b420f413a5077d07164358fb2694cc0100
ChayceJRoss <chayceross108@gmail.com>
2021-03-13 15:09:12 -0800

    new PID values

--- PID/PID.cpp
+++ PID/PID.cpp
@@ -16,7 +16,7 @@ Output Parameters: None
 #define MAX_SPEED 240
 
 int enA = 3;
-int enB = 8;
+int enB = 9;
 
 int in1 = 4;
 int in2 = 5;
@@ -28,9 +28,11 @@ float prev_error_angle = 0;
 
     
 //PID Constants
-float kP = 1;
-float kI = 1;
-float kD = 1;
+float kP = 4;
+float kI = 0;
+float kD = 100;
+
+ float i_error_speed = 0.0;
 
 void motorSetup()
 {
@@ -47,9 +49,9 @@ void motorSetup()
     pinMode(in4, OUTPUT);
 
     //Initial state - motor off
-    digitalWrite(in1, LOW);
+    digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
+    digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
 }
 
@@ -68,14 +70,7 @@ void motorController(float error_angle)
     prev_error_angle = error_angle; 
 
     //Two cases needed for forward movement and reverse movement
-    if (error_angle > 0)
-    {
-        motorReverse();
-    }
-    else
-    {
-        motorForward();
-    }
+
 
     //P:instanteneousError
     float p_error_speed = error_angle * kP;
@@ -83,38 +78,53 @@ void motorController(float error_angle)
     Serial.println(p_error_speed);
     
     //D:rateOFError
-    float d_error_speed = (diff_error_angle)/elapsed_time*kD;
+    float d_error_speed = (diff_error_angle)/(float)elapsed_time*kD;
     Serial.print("D Error: ");
     Serial.println(d_error_speed);
     
 
     //I:cumulativeError
-    float i_error_speed = i_error_speed + (error_angle*elapsed_time)*kI;
-    Serial.print("P Error: ");
+    i_error_speed = i_error_speed + (error_angle)*kI;
+    if (i_error_speed > 0) {
+      i_error_speed = fabs(i_error_speed) < MAX_SPEED ? fabs(i_error_speed) : MAX_SPEED;
+    } else {
+      i_error_speed = fabs(i_error_speed) < MAX_SPEED ? fabs(i_error_speed) : MAX_SPEED;
+      i_error_speed = i_error_speed * -1.0;
+    }
+    
+    
+    Serial.print("I Error: ");
     Serial.println(i_error_speed);
     
     float error_speed =  p_error_speed + d_error_speed + i_error_speed;
     Serial.print("Error Speed: ");
     Serial.println(error_speed);
     set_motor_speed(error_speed);
-
-
+    if (error_speed > 0)
+    {
+        motorReverse();
+    }
+    else
+    {
+        motorForward();
+    }
+    
 }
 
 void motorForward(){
   Serial.println("Forward - ");
   digitalWrite(in1,HIGH);
   digitalWrite(in2,LOW);
-  digitalWrite(in3,HIGH);
-  digitalWrite(in4,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
 }
 
 void motorReverse(){
   Serial.println("Backward - ");
   digitalWrite(in1,LOW);
   digitalWrite(in2,HIGH);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,HIGH);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
 }
 
 void set_motor_speed(float error_speed){
